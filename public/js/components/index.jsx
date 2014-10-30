167f73f93578f14845ac839479bb1be2bcae1288
Kenneth Chung <ken70r@gmail.com>
2014-10-29 21:47:09 -0700

    move index route to its own file
    also remove randomcolor dependency and use hsl() instead

---
+++ public/js/components/index.jsx
@@ -0,0 +1,185 @@
+var React     = require('react');
+require('react/addons');
+var Appconfig = require('../appconfig');
+var Link      = require('react-router').Link;
+var Markdown  = require('pagedown');
+var moment    = require('moment');
+
+function transformSnapshotToNote(snapshot) {
+  var note = snapshot.val();
+  note.name = snapshot.name();
+  note.content = Markdown.getSanitizingConverter().makeHtml(note.content);
+  note.createdAt = new Date(note.createdAt);
+  note.localHidden = note.hidden;
+  note.style = { background: 'hsl(' + Math.floor(Math.random()*360) + ',100%,87.5%)' };
+  return note;
+}
+
+function escapeRegexp(str) {
+  return str.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&");
+}
+
+var Note = React.createClass({
+  toggleLocalHidden: function(e) {
+    if (window.getSelection().toString() || e.target.tagName.match(/^[ai]$/i)) return;
+    this.props.onToggleLocalHidden();
+  },
+
+  render: function() {
+    var note = this.props.note;
+    var cx = React.addons.classSet;
+    var noteContentClasses = cx({
+      'note-content': true,
+      'hidden': note.localHidden,
+    });
+
+    return (
+      <li className="note" onClick={this.toggleLocalHidden} style={note.style}>
+        <div className="controls">
+          <time>{moment(note.createdAt).fromNow().replace(' ago', '')}</time>
+          <div className="icons">
+            <a href="#" onClick={this.props.onToggleHidden}>{note.hidden ? '☼' : '☀'}</a>
+            &nbsp;
+            <a href="#" className="delete-note" onClick={this.props.onDelete}>✖</a>
+          </div>
+        </div>
+        <div className={noteContentClasses} dangerouslySetInnerHTML={{__html: note.content}} />
+      </li>
+    );
+  }
+});
+
+var Index = React.createClass({
+  mixins: [React.addons.LinkedStateMixin],
+
+  statics: {
+    willTransitionTo: function(transition) {
+      if (Appconfig.authRequired && !Appconfig.user) {
+        transition.redirect('login');
+      }
+    },
+  },
+
+  getInitialState: function() {
+    return { notes: [], newNote: '', filter: '' };
+  },
+
+  componentWillMount: function() {
+    this.firebaseRef = Appconfig.firebaseRef.child('notes');
+
+    this.firebaseRef.on('child_added', function(snapshot) {
+      this.state.notes.push(transformSnapshotToNote(snapshot));
+      this.state.notes.sort(function(a, b) {
+        return a.createdAt > b.createdAt ? -1 : 1;
+      });
+      this.setState({ notes: this.state.notes });
+    }.bind(this));
+
+    this.firebaseRef.on('child_removed', function(snapshot) {
+      var snapshotName = snapshot.name();
+      var newNotesList = this.state.notes.filter(function(note) {
+        return note.name !== snapshotName;
+      });
+      this.setState({ notes: newNotesList });
+    }.bind(this));
+
+    this.firebaseRef.on('child_changed', function(snapshot) {
+      var snapshotName = snapshot.name();
+      var note, i;
+      for (i = 0; i < this.state.notes.length; i++) {
+        if (this.state.notes[i].name === snapshotName) {
+          note = this.state.notes[i];
+          break;
+        }
+      }
+      note.hidden = note.localHidden = snapshot.val().hidden;
+      this.setState({ notes: this.state.notes });
+    }.bind(this));
+  },
+
+  updateNewNote: function() {
+    this.setState({ newNote: this.refs.textarea.getDOMNode().value });
+  },
+
+  submitNewNote: function() {
+    if (!this.state.newNote) {
+      return;
+    }
+
+    this.firebaseRef.push({
+      content: this.state.newNote,
+      createdAt: (new Date()).toISOString(),
+      hidden: false,
+    });
+
+    this.setState({ newNote: '' });
+  },
+
+  handleKeyDown: function(e) {
+    if (e.keyCode == 13 && e.ctrlKey) {
+      this.submitNewNote();
+      e.preventDefault(); // prevents new line from pressing enter
+    }
+  },
+
+  toggleLocalHidden: function(note) {
+    note.localHidden = !note.localHidden;
+    this.setState({ notes: this.state.notes });
+  },
+
+  toggleHidden: function(note) {
+    this.firebaseRef.child(note.name).update({ hidden: !note.hidden });
+  },
+
+  delete: function(note) {
+    this.firebaseRef.child(note.name).remove();
+  },
+
+  render: function() {
+    var notes = this.state.notes;
+
+    if (this.state.filter) {
+      var filterRegexp = new RegExp(escapeRegexp(this.state.filter), 'i');
+      notes = notes.filter(function(note) {
+        return note.content.match(filterRegexp);
+      });
+    }
+
+    var logoutLink;
+    if (Appconfig.user) {
+      logoutLink = <Link to="logout" className="logout-button">Logout</Link>;
+    }
+
+    return (
+      <div id="wrapper">
+        <aside>
+          <div>
+            <textarea valueLink={this.linkState('newNote')} onKeyDown={this.handleKeyDown}></textarea>
+            <button onClick={this.submitNewNote}>Post</button>
+          </div>
+
+          <div>
+            <input type="text" valueLink={this.linkState('filter')} />
+          </div>
+
+          {logoutLink}
+        </aside>
+
+        <ul>
+          <li>Notes: {this.state.notes.length}</li>
+          {notes.map(function(note) {
+            return (
+              <Note note={note}
+                    onToggleLocalHidden={this.toggleLocalHidden.bind(this, note)}
+                    onToggleHidden={this.toggleHidden.bind(this, note)}
+                    onDelete={this.delete.bind(this, note)} />
+            );
+          }, this)}
+          <li>Notes: {this.state.notes.length}</li>
+        </ul>
+      </div>
+    );
+  },
+});
+
+module.exports = Index;
