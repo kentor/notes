76935a91ece76a800c8ea6be910836e742353147
Kenneth Chung <ken70r@gmail.com>
2014-12-15 20:59:38 -0800

    Reorder and rename tasks for gulpfile
    Separate watch tasks from build tasks

--- gulpfile.js
+++ gulpfile.js
@@ -17,7 +17,28 @@ gulp.task('express', function() {
   app.listen(4069);
 });
 
-gulp.task('scripts', function() {
+gulp.task('build', function() {
+  return browserify('./src/js/app.js')
+    .bundle()
+    .pipe(source('app.js'))
+    .pipe(buffer())
+    .pipe(uglify())
+    .pipe(gulp.dest('./public/js/'));
+});
+
+gulp.task('css', function() {
+  gulp.src('src/css/app.styl')
+    .pipe(stylus({
+      'include css': true,
+    }))
+    .pipe(gulp.dest('public/css/'));
+});
+
+gulp.task('watch-css', ['css'], function() {
+  gulp.watch('src/css/**/*.styl', ['css']);
+});
+
+gulp.task('watch-js', function() {
   watchify.args.debug = true;
   var bundler = watchify(browserify('./src/js/app.js', watchify.args));
 
@@ -41,53 +62,34 @@ var LINT = [
   'src/js/**/*.js',
   '!src/js/app.js',
 ];
+
 gulp.task('lint', function() {
   gulp.src(LINT)
     .pipe(jshint())
     .pipe(jshint.reporter(require('jshint-stylish')));
 });
 
+gulp.task('watch-lint', ['lint'], function() {
+  gulp.watch(LINT, ['lint']);
+});
+
 var tinylr;
+
 gulp.task('livereload', function() {
   tinylr = livereload();
   tinylr.listen(4070);
 });
 
-function notifyLiveReload(event) {
-  var fileName = require('path').relative(__dirname, event.path);
-  tinylr.changed({
-    body: {
-      files: [fileName]
-    }
+gulp.task('watch-livereload', ['livereload'], function() {
+  gulp.watch('public/**/*', function(event) {
+    var filename = require('path').relative(__dirname, event.path);
+    tinylr.changed({
+      body: {
+        files: [filename],
+      },
+    });
   });
-}
-
-gulp.task('watch', function() {
-  gulp.watch('public/*.html', notifyLiveReload);
-  gulp.watch('public/js/app.js', notifyLiveReload);
-  gulp.watch(LINT, ['lint']);
 });
 
-gulp.task('build', function() {
-  return browserify('./src/js/app.js')
-    .bundle()
-    .pipe(source('app.js'))
-    .pipe(buffer())
-    .pipe(uglify())
-    .pipe(gulp.dest('./public/js/'));
-});
-
-gulp.task('css', function() {
-  gulp.src('src/css/app.styl')
-    .pipe(stylus({
-      'include css': true,
-    }))
-    .pipe(gulp.dest('public/css/'));
-});
-
-gulp.task('watch-css', ['css'], function() {
-  gulp.watch('src/css/**/*.styl', ['css']);
-});
-
-gulp.task('default', ['express', 'scripts', 'lint', 'livereload',
-                      'watch-css', 'watch']);
+gulp.task('default', ['express', 'watch-css', 'watch-js', 'watch-lint',
+                      'watch-livereload']);
