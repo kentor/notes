6a09638ff3454ebaa7960e4cfb225278a20e206a
Kenneth Chung <ken70r@gmail.com>
2015-01-02 00:09:28 -0800

    Add 1 year maxAge to static files
    Except index.html since we need that to serve the latest assets. We need
    to use `send` to serve `index.html` to control the max age for that
    path, otherwise it will be given a max age of 1 year from now.

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 var compression = require('compression');
 var express = require('express');
 var morgan = require('morgan');
+var send = require('send');
 
 var app = express();
 
 app.use(compression());
-app.use(express.static(__dirname + '/public'));
 app.use(morgan('dev'));
+
+app.get('/', function(req, res) {
+  send(req, 'index.html', {
+    maxAge: 0,
+    root: __dirname + '/public',
+  }).pipe(res);
+});
+
+app.use(express.static(__dirname + '/public', {maxAge: 31536000000 }));
 
 var port = process.env.PORT || 9292;
 app.listen(port, function() {
