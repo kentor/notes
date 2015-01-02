c45f08a5c81a148bed55a7b588f63681a2cb7099
Kenneth Chung <ken70r@gmail.com>
2015-01-01 23:39:02 -0800

    Use compression middleware for gzip
    This will gzip our static files before sending them back in the
    response

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+var compression = require('compression');
 var express = require('express');
 var morgan = require('morgan');
 
 var app = express();
 
+app.use(compression());
 app.use(express.static(__dirname + '/public'));
 app.use(morgan('dev'));
 
