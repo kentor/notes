2af25f7e4462d1a5929ec6942eb17ea911d97d73
Kenneth Chung <ken70r@gmail.com>
2015-01-04 22:04:40 -0800

    Use 6to5 for jest

--- __tests__/note-test.js
+++ __tests__/note-test.js
@@ -3,8 +3,8 @@ jest.dontMock('../src/js/models/note');
 var Immutable = require('immutable');
 var Note = require('../src/js/models/note');
 
-describe('note', function() {
-  it('returns object without localHidden when toJS called', function() {
+describe('note', () => {
+  it('returns object without localHidden when toJS called', () => {
     var note = new Note({ localHidden: 'sup' });
     expect(Immutable.Record.prototype.toJS.call(note).localHidden).toBe('sup');
     expect(note.toJS().localHidden).toBe(undefined);
