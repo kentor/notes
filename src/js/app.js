1cf9f68f072515d7f907ea672c34ff0d76fc4572
Kenneth Chung <ken70r@gmail.com>
2015-01-06 20:19:04 -0800

    Use DefaultRoute to prevent redirecting to `/#/index`
    With DefaultRoute we stay at `/#/` instead of `/#/index`

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Router from 'react-router';
-import { Route } from 'react-router';
+import { DefaultRoute, Route } from 'react-router';
 
 import App from './components/app';
 import Index from './components/index';
@@ -9,7 +9,7 @@ import Logout from './components/logout';
 
 Router.run((
   <Route name="app" path="/" handler={App}>
-    <Route name="index" handler={Index} />
+    <DefaultRoute name="index" handler={Index} />
     <Route name="login" handler={Login} />
     <Route name="logout" handler={Logout} />
   </Route>
