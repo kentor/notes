6cace6a8398089ef3fbd30a6da5034add70691ab
Kenneth <kenneth@dirtybit.dev>
2022-06-24 19:04:54 -0700

    Fix indentation

--- src/js/components/Note.tsx
+++ src/js/components/Note.tsx
@@ -1,6 +1,6 @@
 import Icon from 'App/components/Icon';
 import marked from 'marked';
-import React, {ForwardedRef, useState} from 'react';
+import React, {useState} from 'react';
 import TimeAgo from 'react-timeago';
 import {deleteNote, updateNote} from 'App/api';
 import {getBackgroundStyles} from 'App/lib/stationery';
@@ -21,84 +21,82 @@ function timeAgoFormatter(value: number, unit: string) {
   return `${value} ${unit}${value === 1 ? '' : 's'}`;
 }
 
-const Note = React.forwardRef(
-  (props: Props, ref: ForwardedRef<HTMLDivElement>) => {
-    const {note} = props;
+const Note = React.forwardRef<HTMLDivElement, Props>((props, ref) => {
+  const {note} = props;
 
-    const contentDiv = React.createRef<HTMLDivElement>();
+  const contentDiv = React.createRef<HTMLDivElement>();
 
-    const [localHidden, setLocalHidden] = useState(note.hidden);
+  const [localHidden, setLocalHidden] = useState(note.hidden);
 
-    async function handleCopy(e: React.MouseEvent) {
-      e.stopPropagation();
-      try {
-        await navigator.clipboard.writeText(note.content);
-      } catch (e) {
-        console.error('Failed copying note', e);
-      }
+  async function handleCopy(e: React.MouseEvent) {
+    e.stopPropagation();
+    try {
+      await navigator.clipboard.writeText(note.content);
+    } catch (e) {
+      console.error('Failed copying note', e);
     }
+  }
 
-    function handleDelete(e: React.MouseEvent) {
-      e.stopPropagation();
-      deleteNote(note.id);
+  function handleDelete(e: React.MouseEvent) {
+    e.stopPropagation();
+    deleteNote(note.id);
+  }
+
+  function handleToggleHidden(e: React.MouseEvent) {
+    e.stopPropagation();
+    updateNote(note.id, {hidden: !note.hidden});
+    setLocalHidden(!note.hidden);
+  }
+
+  function handleToggleLocalHidden(e: React.MouseEvent) {
+    if (window.getSelection()?.toString()) return;
+    if (e.target instanceof HTMLElement && e.target.tagName === 'A') {
+      return;
     }
+    setLocalHidden(!localHidden);
+  }
 
-    function handleToggleHidden(e: React.MouseEvent) {
-      e.stopPropagation();
-      updateNote(note.id, {hidden: !note.hidden});
-      setLocalHidden(!note.hidden);
-    }
-
-    function handleToggleLocalHidden(e: React.MouseEvent) {
-      if (window.getSelection()?.toString()) return;
-      if (e.target instanceof HTMLElement && e.target.tagName === 'A') {
-        return;
-      }
-      setLocalHidden(!localHidden);
-    }
-
-    return (
+  return (
+    <div
+      className="note"
+      onClick={handleToggleLocalHidden}
+      style={{
+        display: props.visible ? undefined : 'none',
+        ...getBackgroundStyles(note.id),
+        ...props.style,
+      }}
+      ref={ref}
+    >
       <div
-        className="note"
-        onClick={handleToggleLocalHidden}
         style={{
-          display: props.visible ? undefined : 'none',
-          ...getBackgroundStyles(note.id),
-          ...props.style,
+          display: 'flex',
+          fontSize: 11,
+          justifyContent: 'space-between',
         }}
-        ref={ref}
       >
-        <div
-          style={{
-            display: 'flex',
-            fontSize: 11,
-            justifyContent: 'space-between',
-          }}
-        >
-          <TimeAgo date={note.createdAt} formatter={timeAgoFormatter} />
-          <div>
-            <a onClick={handleToggleHidden}>
-              <Icon icon={note.hidden ? 'star-outline' : 'star'} />
-            </a>{' '}
-            <a onClick={handleDelete}>
-              <Icon icon="trashcan" />
-            </a>{' '}
-            <a onClick={handleCopy}>
-              <Icon icon="clipboard" />
-            </a>
-          </div>
-        </div>
-        <div style={{display: localHidden ? 'none' : undefined}}>
-          <div
-            dangerouslySetInnerHTML={{
-              __html: marked.inlineLexer(note.content, []),
-            }}
-            ref={contentDiv}
-          />
+        <TimeAgo date={note.createdAt} formatter={timeAgoFormatter} />
+        <div>
+          <a onClick={handleToggleHidden}>
+            <Icon icon={note.hidden ? 'star-outline' : 'star'} />
+          </a>{' '}
+          <a onClick={handleDelete}>
+            <Icon icon="trashcan" />
+          </a>{' '}
+          <a onClick={handleCopy}>
+            <Icon icon="clipboard" />
+          </a>
         </div>
       </div>
-    );
-  },
-);
+      <div style={{display: localHidden ? 'none' : undefined}}>
+        <div
+          dangerouslySetInnerHTML={{
+            __html: marked.inlineLexer(note.content, []),
+          }}
+          ref={contentDiv}
+        />
+      </div>
+    </div>
+  );
+});
 
 export default Note;
