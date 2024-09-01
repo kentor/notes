6a61c9a86439b5a36b9d81ce0bb83de63211e96b
Kenneth <ken70r@gmail.com>
2024-09-01 16:23:56 -0700

    Add yt title extractor

--- src/js/api.tsx
+++ src/js/api.tsx
@@ -4,14 +4,33 @@ import {tx, id} from '@instantdb/react';
 
 export const authRequired = true;
 
-export function createNote(content: string) {
-  db.transact([
-    tx.notes[id()].update({
-      content,
-      created_at: new Date().toISOString(),
-      hidden: false,
-    }),
-  ]);
+export async function createNote(content: string) {
+  const newId = id();
+  try {
+    await db.transact([
+      tx.notes[newId].update({
+        content,
+        created_at: new Date().toISOString(),
+        hidden: false,
+      }),
+    ]);
+    if (content.match(/youtu\.?be/)) {
+      const response = await fetch(
+        `https://get-title.deno.dev/?url=${content}`,
+      );
+      if (!response.ok) {
+        return;
+      }
+      const {title} = await response.json();
+      if (title) {
+        db.transact([
+          tx.notes[newId].update({
+            content: `${content} ${title}`,
+          }),
+        ]);
+      }
+    }
+  } catch (e) {}
 }
 
 export function updateNote(id: string, updates: Pick<Note, 'hidden'>) {
